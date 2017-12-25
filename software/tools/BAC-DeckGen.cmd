084926efd8992501438376ef5e75579eae1277ab
Paul Kimpel <paul.kimpel@digm.com>
2017-12-25 13:34:08 -0800

    Commit BALGOL Compiler and Generator WIP. Generator assembles, matches transcribed listing, and appears to work. Compiler runs but has major problems -- additional proofing of transcription required.

--- software/tools/BAC-DeckGen.cmd
+++ software/tools/BAC-DeckGen.cmd
@@ -1,34 +1,34 @@
 rem Generate BAC-Assembler card decks from BALGOL transcription files.
 pushd ..\BALGOL
-..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Main.baca
-..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Overlay.baca
-..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Intrinsics\ACOS.baca
-..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Intrinsics\ASIN.baca
-..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Intrinsics\ATAN.baca
-..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Intrinsics\COS.baca
-..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Intrinsics\COSH.baca
-..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Intrinsics\ENTIR.baca
-..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Intrinsics\ERROR.baca
-..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Intrinsics\EXP.baca
-..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Intrinsics\FIX.baca
-..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Intrinsics\FLFL.baca
-..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Intrinsics\FLFX.baca
-..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Intrinsics\FLOAT.baca
-..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Intrinsics\FXFL.baca
-..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Intrinsics\FXFX.baca
-..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Intrinsics\LABEL.baca
-..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Intrinsics\LOG.baca
-..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Intrinsics\MONTR.baca
-..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Intrinsics\READ.baca
-..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Intrinsics\REED.baca
-..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Intrinsics\RITE.baca
-..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Intrinsics\ROMXX.baca
-..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Intrinsics\SIN.baca
-..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Intrinsics\SINH.baca
-..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Intrinsics\SQRT.baca
-..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Intrinsics\TAN.baca
-..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Intrinsics\TANH.baca
-..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Intrinsics\TRACE.baca
-..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Intrinsics\WRITE.baca
+rem ..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Main.baca
+rem ..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Overlay.baca
+..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Library\ACOS.baca
+..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Library\ASIN.baca
+..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Library\ATAN.baca
+..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Library\COS.baca
+..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Library\COSH.baca
+..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Library\ENTIR.baca
+..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Library\ERROR.baca
+..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Library\EXP.baca
+..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Library\FIX.baca
+..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Library\FLFL.baca
+..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Library\FLFX.baca
+..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Library\FLOAT.baca
+..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Library\FXFL.baca
+..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Library\FXFX.baca
+..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Library\LABEL.baca
+..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Library\LOG.baca
+..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Library\MONIT.baca
+..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Library\READ.baca
+..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Library\REED.baca
+..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Library\RITE.baca
+..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Library\ROMXX.baca
+..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Library\SIN.baca
+..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Library\SINH.baca
+..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Library\SQRT.baca
+..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Library\TAN.baca
+..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Library\TANH.baca
+..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Library\TRACE.baca
+..\tools\BAC-Xscript-Reformatter.wsf /q BALGOL-Library\WRITE.baca
 rem Finish BALGOL deck generation.
 popd
--- software/tools/README.txt
+++ software/tools/README.txt
@@ -1,27 +1,33 @@
 Index of folder retro-220/software/tools:
 Scripts and utilities supporting the Burroughs 220 BALGOL Compiler
-recovery effort.
+reconstruction effort.
 
 Unless otherwise specified, all files are in standard Windows text
 format, with carriage-return/line-feed delimiters.
 
 
-    [WORK IN PROGRESS]
-
-
 BAC-Assembler.html
     HTML/Javascript assembler for the assembly language dialect used
-    with the BALGOL Main, Overlay, and intrinsic functions.
+    with the BALGOL Main, Overlay, and library functions.
+
+BAC-DeckGen.cmd
+    Windows command-line script to generate card decks for BAC-Assembler
+    from the transcribed *.baca files for the BALGOL compiler.
 
 BAC-XScript-Reformatter.wsf
     Windows VBScript utility to extract source code from the BALGOL
     assembly listing transcriptions and reformat them into card decks
     for use by BAC-Assembler.html
 
-BAC-DeckGen.cmd
-    Windows command-line script to generate card decks for BAC-Assembler
-    from the transcribed *.baca files for the BALGOL compiler.
+BAC-Assembler.html
+    HTML/Javascript assembler for the assembly language dialect used
+    with the BALGOL Generator utility.
+
+GEN-XScript-Reformatter.wsf
+    Windows VBScript utility to extract source code from the Generator
+    assembly listing transcription and reformat it into a card deck for
+    use by GEN-Assembler.html
 
 Paul Kimpel
-March 2017
+December 2017
 
